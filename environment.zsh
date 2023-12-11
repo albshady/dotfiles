cd485e178a8ea7ebd777c1bfc4fdd209b8611af0
albshady <3litac@gmail.com>
2023-12-11 15:41:33 +0100

    🚀 Set docker-specific env vars

--- environment.zsh
+++ environment.zsh
@@ -9,5 +9,13 @@ export BAT_THEME=Dracula
 export EDITOR=nvim
 export KUBECTL_IN_PROMPT_FILENAME='.show-prompt.k8s'
 
+# pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+
+# docker
+export DOCKER_BUILDKIT='1'
+# set proper DOCKER_HOST when using colima runtime
+if command -v colima &> /dev/null; then
+    export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+fi
